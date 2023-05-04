7e8de53dcccc171771f858270142da85374e3571
sam_ <45014739+sbordel@users.noreply.github.com>
2023-05-04 16:37:51 -0400

    reformatted JSON + parsing data into variables

--- EGGS_/js/script.js
+++ EGGS_/js/script.js
@@ -1,6 +1,10 @@
 
-let recipeData;
+let recipeData = [];
 let recipeTitle;
+let recipeType;
+let recipeIng = [];
+let recipeSteps = [];
+let recipeURL;
 let idNum;
 
 let eggId;
@@ -11,21 +15,37 @@ let Arraysrc;
 
 $(document).ready(function () {
 
-  $.getJSON("./json/recipes.json", function (data) {
+  $.getJSON("../json/recipes.json", function (data) {
     dataFromJSON = data;
     for (let j = 0; j < dataFromJSON.length; j++) {
     }
-    recipeData = dataFromJSON.recipes;
-    run();
+    recipeData = dataFromJSON.Recipes;
+
+    for (let i = 0; i < recipeData.length; i++) {
+      recipeTitle = recipeData[i].Title;
+      recipeType = recipeData[i].Type;
+      recipeIng = recipeData[i].Ingredients;
+      recipeSteps = recipeData[i].Steps;
+      recipeURL = recipeData[i].OriginalURL;
+    }
   });
 
 
-  function run() {
-    console.log(recipeData[0]);
-  };
+  // function run() {
+  //   recipeData;
+  //   for (let i = 0; i < recipeData.length; i++) {
+  //     recipeTitle = recipeData[i].Title;
+  //     recipeType = recipeData[i].Type;
+  //     recipeIng = recipeData[i].Ingredients;
+  //     recipeSteps = recipeData[i].Steps;
+  //     recipeURL = recipeData[i].OriginalURL;
+  //   }
+  //   console.log(recipeTitle[2]);
+  // };
 
   $(".eggimg").on({
     'click': function () {
+      console.log(recipeData[].Type);
       eggId = $(this).attr("id");
       eggImg = document.getElementsByClassName("eggimg");
       eggImg.src = "assets/egg" + eggId + ".gif";
@@ -40,21 +60,21 @@ $(document).ready(function () {
       this.src = eggArray[eggId].src;
 
       setTimeout(animEgg, 4500, "done");
-      
+
       function animEgg(p1) {
         eggImg.src = "assets/egg" + eggId + ".png";
         console.log(p1);
       }
-      }
-      });
+    }
+  });
 
-      // function change() {
-      //   var elem = $("#btn-a");
-      //   var list = $("#breakfast-a")
-      //   if (elem.value == "▾") elem.value = "▸", list.style.display = "none";
-      //   else elem.value = "▾", list.style.display = "inline";
-      // }
+  // function change() {
+  //   var elem = $("#btn-a");
+  //   var list = $("#breakfast-a")
+  //   if (elem.value == "▾") elem.value = "▸", list.style.display = "none";
+  //   else elem.value = "▾", list.style.display = "inline";
+  // }
 
 
-    });
+});
 
