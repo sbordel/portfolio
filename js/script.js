3797e8e33802d2e9a7563a5a3c7e59ef1a582eae
sam_ <45014739+sbordel@users.noreply.github.com>
2024-01-25 13:28:01 -0500

    added json and modified page to have project description pop-up

--- js/script.js
+++ js/script.js
@@ -1,152 +1,264 @@
-let prjContent;
-let infoContent;
+var prjData = [];
+
+var prjId;
+var prjTitle;
+var prjLink;
+var pageTitle;
+var pageLink;
+var pageImg;
+var pageDscA;
+var pageDscB;
+var pageType;
+
+var prjContent;
+var infoContent;
+
+var imgArray = [];
+
+var i;
 
 $(document).ready(function () {
+  "use strict";
+
+  $.getJSON("../json/web-info.json", function (data) {
+    var dataFromJSON = data;
+    for (let j = 0; j < dataFromJSON.length; j++) {
+    }
+    prjData = dataFromJSON.Projects;
+    run();
+  });
+
+  function run() {
+    console.log("json file loaded");
+  };
+
+  let navBody = document.getElementById('nav-body');
+  let navLink = document.getElementsByClassName('nav-link')
+  let navBtn = document.getElementsByClassName('nav-btn');
+
+  pageImg = document.getElementById("content-img");
+  pageTitle = document.getElementById("title");
+  pageDscA = document.getElementById("dsc-brief");
+  pageDscB = document.getElementById("dsc-prompt");
+
+  $("body").on("mousemove", function (event) {
+    var winHeight = window.innerHeight;
+
+    var mouseX = event.pageX;
+    var mouseY = event.pageY;
+
+    let posX = mouseX + 20;
+    let posY;
+
+    if (mouseY >= (winHeight / 2)) {
+      posY = mouseY - (winHeight / 2);
+    } else {
+      posY = mouseY - 50;
+    }
+
+    $(".content-body").css({
+      "left": posX + "px",
+      "top": posY + "px"
+    })
+
+    $(".p-title").on("click", function () {
+      let strId = $(this).attr("id");
+      let idArray = strId.split("-");
+
+      let pageId = idArray[1];
+      let objId = pageId - 1;
+
+      let prjExt;
+
+      if (pageId <= 15) {
+        prjExt = ".jpg";
+      } else if (pageId == 16 || pageId == 17) {
+        prjExt = ".png";
+      } else if (pageId == 18) {
+        prjExt = ".gif";
+      } else {
+        prjExt = "";
+      }
+
+      for (i = 0; i < 18; i++) {
+        imgArray[i] = new Image();
+        imgArray[i].src = "assets/web-shots/screenshot-" + pageId + prjExt;
+      }
+
+      $(pageImg).attr("src", imgArray[objId].src);
+      $(pageTitle).text(prjData[objId].Title + " (" + prjData[objId].Date + ")");
+      $(pageDscA).html(prjData[objId].Description.Brief);
+      $(pageDscB).html(prjData[objId].Description.Prompt);
+      $(".content-body").show();
+    })
+  });
+
   prjContent = `<div class="nav-content" id="prj">
   <table>
   <tr>
     <td class="p-type"><span title="personal project">‧</span></td>
-    <td class="p-title">egg <a class="p-link" href="https://sbordel.github.io/EGG/" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-1"><span class="p-popup">egg</span></td>
+    <td class="p-link"><a href="https://sbordel.github.io/EGG/" target="_blank">⭷</a></td>
     <td class="p-year">2023</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, JSON)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal project">‧</span></td>
-    <td class="p-title">sunday sites [index] <a class="p-link" href="../sunday-sites/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-2"><span class="p-popup">sunday sites [index]</span></td>
+    <td class="p-link"><a href="../sunday-sites/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2022-2023</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal project">‧</span></td>
-    <td class="p-title">ephemerality (#26) <a class="p-link" href="../sunday-sites/site02.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-3"><span class="p-popup">ephemerality (#26)</span></td>
+    <td class="p-link"><a href="../sunday-sites/site02.html" target="_blank">⭷</a></td>
     <td class="p-year">2022</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal project">‧</span></td>
-    <td class="p-title">make a site inspired by a memory (#30) <a class="p-link" href="../sunday-sites/site01.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-4"><span class="p-popup">make a site inspired by a memory (#30)</span></td>
+    <td class="p-link"><a href="../sunday-sites/site01.html" target="_blank">⭷</a></td>
     <td class="p-year">2023</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="commission project">⁝</span></td>
-    <td class="p-title">banshee <a class="p-link" href="https://bansheenight.club/" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-5"><span class="p-popup">banshee</span></td>
+    <td class="p-link"><a href="https://bansheenight.club/" target="_blank">⭷</a></td>
     <td class="p-year">2022</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">mini websites [index] <a class="p-link" href="../misc-websites/entry.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-6"><span class="p-popup">mini websites [index]</span></td>
+    <td class="p-link"><a href="../misc-websites/entry.html" target="_blank">⭷</a></td>
     <td class="p-year">2020-2022</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, jQuery)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">friends only <a class="p-link" href="../misc-websites/projects/friends-only/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-7"><span class="p-popup">friends only</span></td>
+    <td class="p-link"><a href="../misc-websites/projects/friends-only/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, jQuery)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">fruit salad <a class="p-link" href="../misc-websites/projects/fruit-salad/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-8"><span class="p-popup">fruit salad</span></td>
+    <td class="p-link"><a href="../misc-websites/projects/fruit-salad/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, jQuery UI)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">haiku best gill <a class="p-link" href="../misc-websites/projects/haiku-best-gill/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-9"><span class="p-popup">haiku best gill</span></td>
+    <td class="p-link"><a href="../misc-websites/projects/haiku-best-gill/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">mood <a class="p-link" href="../misc-websites/projects/mood/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-11"><span class="p-popup">mood</span></td>
+    <td class="p-link"><a href="../misc-websites/projects/mood/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2022</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, jQuery)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">pizza palooza <a class="p-link" href="https://hybrid.concordia.ca/s_bordel/exercises/exercise_03/exercise_e3.php" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-12"><span class="p-popup">pizza palooza </span></td>
+    <td class="p-link"><a href="https://hybrid.concordia.ca/s_bordel/exercises/exercise_03/exercise_e3.php" target="_blank">⭷</a></td>
     <td class="p-year">2022</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, jQuery, AJAX, PHP)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">sonny-jim <a class="p-link" href="../misc-websites/projects/sonny-jim/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-13"><span class="p-popup">sonny-jim</span></td>
+    <td class="p-link"><a href="../misc-websites/projects/sonny-jim/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, p5.js, Three.js)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="school project">∶</span></td>
-    <td class="p-title">where's mister rabbit? <a class="p-link" href="../misc-websites/projects/mister-rabbit/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-14"><span class="p-popup">where's mister rabbit?</span></td>
+    <td class="p-link"><a href="../misc-websites/projects/mister-rabbit/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">web</td>
     <td class="p-role">graphic design + front end dev (HTML/CSS, Javascript, p5.js)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="collaboration project">⁖</span></td>
-    <td class="p-title">imca raydeeoh <a class="p-link" href="../imca-raydeeoh/index.html" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-15"><span class="p-popup">imca raydeeoh</span></td>
+    <td class="p-link"><a href="../imca-raydeeoh/index.html" target="_blank">⭷</a></td>
     <td class="p-year">2019-2023</td>
     <td class="p-medium">web</td>
     <td class="p-role">project management + graphic design + front end dev (HTML/CSS)</td>
   </tr>
   <tr>
     <td class="p-type"><span title="coordination/personal project">⋰</span></td>
-    <td class="p-title">2024 calendar <a class="p-link" href="" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-16"><span class="p-popup">2024 calendar</span></td>
+    <td class="p-link"><a href="https://www.zeffy.com/en-CA/ticketing/1a000fe8-ca5e-4ba8-a017-41dfe82478a1"  target="_blank">⭷</a></td>
     <td class="p-year">2023</td>
     <td class="p-medium">print</td>
     <td class="p-role">project management + content curation + graphic design</td>
   </tr>
   <tr>
     <td class="p-type"><span title="coordination/personal project">⋰</span></td>
-    <td class="p-title">2023 calendar <a class="p-link" href="" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-17"><span class="p-popup">2023 calendar</span></td>
+    <td class="p-link"><a href="#">⭷</a></td>
     <td class="p-year">2022</td>
     <td class="p-medium">print</td>
     <td class="p-role">project management + content curation + graphic design</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal/collaboration project">‧</span></td>
-    <td class="p-title">weepy daisy <a class="p-link" href="https://drive.google.com/file/d/1YEQCAgzelUNUEY6Hwrysi57_enk6kSy-/view?usp=sharing" target="_blank">⭷</a></td>
+    <td class="p-title" id="n-18"><span class="p-popup">weepy daisy</span></td>
+    <td class="p-link"><a href="https://drive.google.com/file/d/1YEQCAgzelUNUEY6Hwrysi57_enk6kSy-/view?usp=sharing" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">3D</td>
     <td class="p-role">design + 3D modelling + armature animation</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal/contribution project">‧</span></td>
-    <td class="p-title">mix 49: slug bait — <abbr title="radio show">ffiles</abbr> <a class="p-link" href="https://soundcloud.com/ffilesradio/mix-049-for-n10as-slug-bait-by-h2wo?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
+    <td class="p-title">mix 49: slug bait — <abbr title="radio show">ffiles</abbr></td>
+    <td class="p-link"><a href="https://soundcloud.com/ffilesradio/mix-049-for-n10as-slug-bait-by-h2wo?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
     <td class="p-year">2023</td>
     <td class="p-medium">mix</td>
     <td class="p-role">curation + mixing</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal/contribution project">‧</span></td>
-    <td class="p-title">mix 14 — <abbr title="radio show">missed connections</abbr> <a class="p-link" href="https://soundcloud.com/missed-connections-radio/014-h2wo?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
+    <td class="p-title">mix 14 — <abbr title="radio show">missed connections</abbr></td>
+    <td class="p-link"><a href="https://soundcloud.com/missed-connections-radio/014-h2wo?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
     <td class="p-year">2022</td>
     <td class="p-medium">mix</td>
     <td class="p-role">curation + mixing</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal/contribution project">‧</span></td>
-    <td class="p-title">mix 47 — <abbr title="music label">voragine</abbr> <a class="p-link" href="https://soundcloud.com/vor4gine/mixtape47_h2wo?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
+    <td class="p-title">mix 47 — <abbr title="music label">voragine</abbr></td>
+    <td class="p-link"><a href="https://soundcloud.com/vor4gine/mixtape47_h2wo?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">mix</td>
     <td class="p-role">curation + mixing</td>
   </tr>
   <tr>
     <td class="p-type"><span title="personal/contribution project">‧</span></td>
-    <td class="p-title">kvmix022 — <abbr title="collective">coolground</abbr> <a class="p-link" href="https://soundcloud.com/coolgroundmtl/kvmix022-h2w0?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
+    <td class="p-title">kvmix022 — <abbr title="collective">coolground</abbr></td>
+    <td class="p-link"><a href="https://soundcloud.com/coolgroundmtl/kvmix022-h2w0?in=h2wo0/sets/splish-splash" target="_blank">⭷</a></td>
     <td class="p-year">2021</td>
     <td class="p-medium">mix</td>
     <td class="p-role">curation + mixing</td>
@@ -169,11 +281,14 @@ $(document).ready(function () {
   </tr>
 </table></div>`
 
-  let navBody = document.getElementById('nav-body');
-  let navLink = document.getElementsByClassName('nav-link')
-  let navBtn = document.getElementsByClassName('nav-btn');
+  $(navLink).on("click", function () {
+    $(".content-body").hide();
+    $(pageImg).attr("src", "");
+    $(pageTitle).empty();
+    $(pageDscA).empty();
+    $(pageDscB).empty();
+    $(pageType).empty();
 
-  $(navBtn).click(function () {
     $(navBtn).css({ 'color': '#000', 'background-color': 'transparent' });
     $(navLink).css({ 'color': '#000', 'background-color': 'transparent' });
     if ($(this).attr('id') == 'cv-btn') {
@@ -186,4 +301,5 @@ $(document).ready(function () {
       navBody.innerHTML = infoContent;
     } else { navBody.innerHTML = prjContent; };
   });
+
 });
